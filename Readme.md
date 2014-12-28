43fb74792f4f6a5a651af63e32506204a401faa3
David Hays <Dhays90@gmail.com>
2014-12-27 21:06:06 -0600

    First Commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+**TWRP - HTC One (Sprint)**
+
+This is a recovery-only device tree for the HTC One for Sprint (m7whl / m7spr).
+
+**Quick build instructions:**
+
+    . build/envsetup.sh
+    lunch cm_m7spr-eng
+    make recoveryimage
---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Release name
+PRODUCT_RELEASE_NAME := m7spr
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/m7spr/device.mk)
+
+# Device identifier - this must come after all inclusions
+PRODUCT_DEVICE := m7spr
+PRODUCT_NAME := cm_m7spr
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := One
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=m7wls
