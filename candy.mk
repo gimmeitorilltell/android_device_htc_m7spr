f7ec8474066a3fe647f6a00cef2d9c8b8e75c588
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-11-26 20:44:49 -0500

    candify

--- candy.mk
+++ candy.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/candy5/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:5.0.2/LRX22G/499067.6:user/release-keys" \
     PRIVATE_BUILD_DESC="6.23.651.2 CL499067 release-keys"
 
-PRODUCT_NAME := candy5_m7spr
+PRODUCT_NAME := candy_m7spr
 PRODUCT_DEVICE := m7spr
